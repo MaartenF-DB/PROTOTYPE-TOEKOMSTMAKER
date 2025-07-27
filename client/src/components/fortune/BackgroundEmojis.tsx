e04b0f5fcbe6e67c1d232a3980af9f6ecbc62b55
maartenfdebruij <37948768-maartenfdebruij@users.noreply.replit.com>
2025-07-27 15:12:59 +0000

    Restored to '4fa68de01a8f9f5f820175721cb1553b47620c4a'
    Replit-Restored-To: 4fa68de01a8f9f5f820175721cb1553b47620c4a

--- client/src/components/fortune/BackgroundEmojis.tsx
+++ client/src/components/fortune/BackgroundEmojis.tsx
@@ -14,21 +14,21 @@ export function BackgroundEmojis({ sectionType = 'entry' }: BackgroundEmojisProp
     <div className="fixed inset-0 pointer-events-none z-0 opacity-15">
       {/* Removed top-left elements as requested */}
       {/* Top row - avoiding top-left area */}
-      <div className="absolute top-1/5 right-1/5 text-6xl">{emojis[1]}</div>
-      <div className="absolute top-1/4 left-1/2 text-5xl">{emojis[2]}</div>
+      <div className="absolute top-1/5 right-1/5 text-4xl">{emojis[1]}</div>
+      <div className="absolute top-1/4 left-1/2 text-3xl">{emojis[2]}</div>
       
       {/* Middle row */}
-      <div className="absolute top-1/2 right-1/8 text-7xl">{emojis[4]}</div>
+      <div className="absolute top-1/2 right-1/8 text-5xl">{emojis[4]}</div>
       
       {/* Bottom row */}
-      <div className="absolute bottom-1/4 left-1/3 text-6xl">{emojis[5]}</div>
-      <div className="absolute bottom-1/5 right-1/3 text-5xl">{emojis[0]}</div>
-      <div className="absolute bottom-1/6 left-1/2 text-7xl">{emojis[1]}</div>
+      <div className="absolute bottom-1/4 left-1/3 text-4xl">{emojis[5]}</div>
+      <div className="absolute bottom-1/5 right-1/3 text-3xl">{emojis[0]}</div>
+      <div className="absolute bottom-1/6 left-1/2 text-5xl">{emojis[1]}</div>
       
       {/* Additional scattered emojis - avoiding top-left quadrant */}
-      <div className="absolute top-2/3 right-1/4 text-5xl">{emojis[3]}</div>
-      <div className="absolute bottom-1/3 right-1/5 text-4xl">{emojis[4]}</div>
-      <div className="absolute top-3/4 right-1/6 text-6xl">{emojis[5]}</div>
+      <div className="absolute top-2/3 right-1/4 text-3xl">{emojis[3]}</div>
+      <div className="absolute bottom-1/3 right-1/5 text-2xl">{emojis[4]}</div>
+      <div className="absolute top-3/4 right-1/6 text-4xl">{emojis[5]}</div>
     </div>
   );
 }
